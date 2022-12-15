432f5aa10631e199b7e0825a70fae4b7ca3c2f7b
johnnkp <22496821+johnnkp@users.noreply.github.com>
2022-12-16 00:10:22 +0800

    Add test location demo page

--- app.js
+++ app.js
@@ -39,7 +39,21 @@ const LocationSchema = Schema({
     quota: {type: Number}
 });
 
+const SegmentSchema = mongoose.Schema({
+    // the irn_id should have the same ID as segment_Id in the XML data provided
+    route: {type: String, required: true},
+    irn_id: {type: Number, required: true, unique: true},
+    speed: {type: Number, required: true, default: 0}
+});
+
 const Location = mongoose.model('Location', LocationSchema);
+const Segment = mongoose.model('Segment', SegmentSchema);
+
+let testLocation = ["AUSTIN ROAD WEST", "CANTON ROAD", "CHATHAM ROAD NORTH",
+    "CLEAR WATER BAY ROAD", "CONNAUGHT ROAD CENTRAL", "FERRY STREET",
+    "HING FAT STREET", "HOI ON ROAD", "JOCKEY CLUB ROAD",
+    "LUNG CHEUNG ROAD", "QUEEN'S ROAD EAST", "SALISBURY ROAD",
+    "SHA TIN RURAL COMMITTEE ROAD", "TAI PO TAI WO ROAD", "WATERLOO ROAD"];
 
 // Upon connection failure
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -84,9 +98,16 @@ db.once('open', function () {
     });
 
     app.get('/event', (req, res) => {
-
+        Segment.find({route: {$in: testLocation}}, (err, results) => {
+            // res.send(results);
+        });
     });
 
+    app.get('/lo', (req, res) => {
+        Location.find({name: {$in: testLocation}}, (err, results) => {
+            // res.send(results);
+        });
+    });
 });
 
 module.exports = app;
