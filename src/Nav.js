0fc6df1dd0618fb62fbbad39c0bf4ccf9ccb1841
johnnkp <johnnkp@hotmail.com>
2022-12-16 22:53:06 +0800

    Document: add programmer

--- src/Nav.js
+++ src/Nav.js
@@ -1,3 +1,11 @@
+/* PROGRAMMER:
+ * Mok Chau Wing (1155142763)
+ * Chan Shi Leung Jonathan (1155142863)
+ * Li Tsz Yeung (1155144367)
+ * Ng Kai Pong (1155144829)
+ * Lee Yat Him (1155176301)
+ * Lin Chun Man (1155177065)
+*/
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
--- src/Seperate.js
+++ src/Seperate.js
@@ -1,61 +1,64 @@
+/* PROGRAMMER:
+ * Mok Chau Wing (1155142763)
+ * Chan Shi Leung Jonathan (1155142863)
+ * Li Tsz Yeung (1155144367)
+ * Ng Kai Pong (1155144829)
+ * Lee Yat Him (1155176301)
+ * Lin Chun Man (1155177065)
+*/
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import  { useState, useRef, useEffect } from 'react';
+import './bootstrap.scheme.css';
+import {useState, useRef, useEffect} from 'react';
 import Container from 'react-bootstrap/Container';
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" 
-integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" 
-crossorigin="anonymous"></link>
 
 export default function Seperate() {
 
 
-return(
+    return (
 
-<>
-<Container>
-<div style={{fixed: "left"}}  bg="dark" variant="dark">
-    <table className='table'>
-        <tr>
-            <td className='item'>Road</td>
-            <td>Road name</td>
-        </tr>
-        <tr>
-            <td className='item'>Max Speed</td>
-            <td>100</td>
+        <>
+            <Container>
+                <div style={{fixed: "left"}} bg="dark" variant="dark">
+                    <table className='table'>
+                        <tr>
+                            <td className='item'>Road</td>
+                            <td>Road name</td>
+                        </tr>
+                        <tr>
+                            <td className='item'>Max Speed</td>
+                            <td>100</td>
 
-        </tr>
-        <tr>
-            <td className='item'>Min Speed </td>
-            <td>50</td>
-        </tr>
-    </table>
-    <div></div>
-<div></div>
-<form>
-    <div class="mb-3">
-      <label for="new-email" class="form-label" >Email address</label>
-      <textarea type="email" class="form-control" id="new-email" placeholder="name@example.com"></textarea>
-    </div>
-    <div class="mb-3">
-      <label for="new-comment" class="form-label">Comment</label>
-      
-      <textarea class="form-control" id="new-comment" rows="3"></textarea>
-    
-    
-    </div>
-    <div>
-        <button type= "submit"> submit</button>
-    </div>
-</form>
-</div>
-</Container>
-</>
-)
+                        </tr>
+                        <tr>
+                            <td className='item'>Min Speed</td>
+                            <td>50</td>
+                        </tr>
+                    </table>
+                    <div></div>
+                    <div></div>
+                    <form>
+                        <div className="mb-3">
+                            <label htmlFor="new-email" className="form-label">Email address</label>
+                            <textarea type="email" className="form-control" id="new-email"
+                                      placeholder="name@example.com"></textarea>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="new-comment" className="form-label">Comment</label>
+
+                            <textarea className="form-control" id="new-comment" rows="3"></textarea>
+
+
+                        </div>
+                        <div>
+                            <button type="submit"> submit</button>
+                        </div>
+                    </form>
+                </div>
+            </Container>
+        </>
+    )
 };
 
 
