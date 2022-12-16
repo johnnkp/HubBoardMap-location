047c075e36659292db64eece4e8d189627850de8
johnnkp <johnnkp@hotmail.com>
2022-12-17 04:19:02 +0800

    Login page redesign

--- src/Login.js
+++ src/Login.js
@@ -11,28 +11,32 @@ import {Form, Button} from "react-bootstrap";
 import {useState} from 'react';
 import axios from "axios";
 import loginBackground from "./login/images/bg-01.jpg";
-import "./login/css/main.css"
-import "./login/css/util.css"
+import HubBoardIcon from "./HubBoardMap.svg";
+import loginMainCSS from "./login/css/main.css"
+import loginUtilCSS from "./login/css/util.css"
+import "./bootstrap.scheme.css"
 
 export default function Login() {
     // https://colorlib.com/wp/template/login-form-v5/
     // https://www.freecodecamp.org/news/react-background-image-tutorial-how-to-set-backgroundimage-with-inline-css-style
     return <div className="container-login100" style={{backgroundImage: `url(${loginBackground})`}}>
         <div className="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
-            <form className="login100-form validate-form flex-sb flex-w" action="http://localhost:80/login"
-                  method="post">
-					<span className="login100-form-title p-b-40">
-						Login
+            <form className="login100-form validate-form flex-sb flex-w"
+                  action="http://localhost:80/login" method="post">
+                <div className="d-flex align-items-center w-100 pb-5">
+                    <img src={HubBoardIcon} width="75" height="75"/>
+                    <span className="login100-form-title">
+						HubBoard Map Login
 					</span>
-
-                <label htmlFor="username">Username</label>
+                </div>
+                <label htmlFor="username" className="h5">Username</label>
                 <br/>
                 <div className="wrap-input100 validate-input" data-validate="Username is required">
                     <input className="input100" type="text" id="username" name="username"/>
                     <span className="focus-input100"></span>
                 </div>
 
-                <label htmlFor="password">Password</label>
+                <label htmlFor="password" className="h5 pt-5">Password</label>
                 <br/>
                 <div className="wrap-input100 validate-input" data-validate="Password is required">
                     <input className="input100" type="password" id="password" name="password"/>
