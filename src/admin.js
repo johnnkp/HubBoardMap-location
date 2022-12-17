8e10d102a7bfe68804c7d4fe5a92da49b5881a0f
ryanlincm <120587870+ryanlincm@users.noreply.github.com>
2022-12-17 21:43:04 +0800

    Update admin.js

--- src/admin.js
+++ src/admin.js
@@ -10,6 +10,7 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 import axios, * as others from 'axios';
+import { response } from 'express';
 
 //import Login from "./Login";
 // Experimental: import empty service worker for PWA
@@ -17,9 +18,9 @@ import axios, * as others from 'axios';
 
 let locId = [49, 115, 39, 91, 78, 110, 118, 141, 69, 164, 16, 19, 48, 54, 67];
 
-function App() {
+export default function Admin() {
   return (
-    <BrowserRouter>
+    <div>
       {/* Horizontal Navigation Bar */}
       <Navbar style={{ position: "fixed", height: "10%", width: "100%" }} id="navBar" bg="dark" variant="dark">
         <Container>
@@ -31,92 +32,82 @@ function App() {
           </Navbar.Brand>
         </Container>
       </Navbar>
-      {/* Vertical Navigation Bar */}
-      <div style={{ position: "fixed", height: "90%", width: "20%", top: "10%" }} className="btn-group-vertical">
-        <Link style={{ height: "10%", width: "100%" }} className="btn btn-secondary rounded-0 border border-primary" to="/admin/home">Home</Link>
-        <Link style={{ height: "10%", width: "100%" }} className="btn btn-secondary rounded-0 border border-primary" to="/admin/location_create">Create Location</Link>
-        <Link style={{ height: "10%", width: "100%" }} className="btn btn-secondary rounded-0 border border-primary" to="/admin/location_read">Read Location</Link>
-        <Link style={{ height: "10%", width: "100%" }} className="btn btn-secondary rounded-0 border border-primary" to="/admin/location_update">Update Location</Link>
-        <Link style={{ height: "10%", width: "100%" }} className="btn btn-secondary rounded-0 border border-primary" to="/admin/location_delete">Delete Location</Link>
-        <Link style={{ height: "10%", width: "100%" }} className="btn btn-secondary rounded-0 border border-primary" to="/admin/user_create">Create User</Link>
-        <Link style={{ height: "10%", width: "100%" }} className="btn btn-secondary rounded-0 border border-primary" to="/admin/user_read">Read User</Link>
-        <Link style={{ height: "10%", width: "100%" }} className="btn btn-secondary rounded-0 border border-primary" to="/admin/user_update">Update User</Link>
-        <Link style={{ height: "10%", width: "100%" }} className="btn btn-secondary rounded-0 border border-primary" to="/admin/user_delete">Delete User</Link>
-        <Link style={{ height: "10%", width: "100%" }} className="btn btn-secondary rounded-0 border border-primary" to="/logout">Logout</Link>
-      </div>
-      <Routes>
-        <Route path="/admin/home" element={<Home />} />
-        <Route path="/admin/location_create" element={<CreateLocation />} />
-        <Route path="/admin/location_read" element={<ReadLocation />} />
-        <Route path="/admin/location_update" element={<UpdateLocation />} />
-        <Route path="/admin/location_delete" element={<DeleteLocation />} />
-        <Route path="/admin/user_create" element={<CreateUser/>} />
-        <Route path="/admin/user_read" element={<ReadUser />} />
-        <Route path="/admin/user_update" element={<UpdateUser />} />
-        <Route path="/admin/user_delete" element={<DeleteUser />} />
-      </Routes>
-    </BrowserRouter>
+
+      <div style={{ position: "fixed", height: "90%", width: "20%", top: "10%" }} className="bg-secondary">
+                <button style={{ height: "10%", width: "100%" }}  className="btn btn-secondary rounded-0 border border-primary">Home</button>
+                <button style={{ height: "10%", width: "100%" }} onClick={CreateLocation} className="btn btn-secondary rounded-0 border border-primary">Create Location</button>
+                <button style={{ height: "10%", width: "100%" }} onClick={ReadLocation} className="btn btn-secondary rounded-0 border border-primary">Read Location</button>
+                <button style={{ height: "10%", width: "100%" }} onClick={UpdateLocation} className="btn btn-secondary rounded-0 border border-primary">Update Location</button>
+                <button style={{ height: "10%", width: "100%" }} onClick={DeleteLocation} className="btn btn-secondary rounded-0 border border-primary">Delete Location</button>
+                <button style={{ height: "10%", width: "100%" }} onClick={CreateUser} className="btn btn-secondary rounded-0 border border-primary">Create User</button>
+                <button style={{ height: "10%", width: "100%" }} onClick={ReadUser} className="btn btn-secondary rounded-0 border border-primary">Read User</button>
+                <button style={{ height: "10%", width: "100%" }} onClick={UpdateUser} className="btn btn-secondary rounded-0 border border-primary">Update User</button>
+                <button style={{ height: "10%", width: "100%" }} onClick={DeleteUser} className="btn btn-secondary rounded-0 border border-primary">Delete User</button>
+            </div>
+           <UpdateLocation/>
+           
+            </div>
   );
 }
 
-function Home() {
-  const containerStyle = {
-    width: "80vw",
-    height: "90vh",
-  };
-  const center = {
-    lat: 22.356311,
-    lng: 114.124516
-  };
-  const zoomSize = 11;
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyAe0TdcYNRvHTpj6kL12M3Zbwf_v8WkD8o"
-  });
-  const [map, setMap] = React.useState(null)
-  const onLoad = React.useCallback(function callback(map) {
-    setMap(map)
-  }, [])
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-  // Load Location for Marker
-  let loc = [];
-  for (let i = 0; i < locId.length; i++) {
-    var url = "http://localhost:8000/location/" + locId[i];
-    fetch(url)
-      .then((response) => {
-        response.json()
-      })
-      .then((data) => {
-        for (let i = 0; i < 15; i++) {
-          let obj = { latitude: data[locId[i] - 1].latitude, longtitude: data[locId[i] - 1].longtitude };
-          loc.push(obj);
-        }
-      })
-  }
-  console.log(loc);
-  // Load Google Map
-  return isLoaded ? (
-    <div style={{ position: "absolute", right: "0", bottom: "0" }}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={zoomSize}
-        onLoad={onLoad}
-        onUnmount={onUnmount}>
-        <Marker position={{ lat: 22.356311, lng: 114.124516 }}></Marker>
-        {loc.map((loc) => {
-          // Place Marker on Map
-          return (<Marker position={{ lat: loc.latitude, lng: loc.longtitude }}></Marker>);
-        })}
-      </GoogleMap>
-    </div >
-  ) : <></>
-}
+// function Home() {
+//   const containerStyle = {
+//     width: "80vw",
+//     height: "90vh",
+//   };
+//   const center = {
+//     lat: 22.356311,
+//     lng: 114.124516
+//   };
+//   const zoomSize = 11;
+//   const { isLoaded } = useJsApiLoader({
+//     id: 'google-map-script',
+//     googleMapsApiKey: "AIzaSyAe0TdcYNRvHTpj6kL12M3Zbwf_v8WkD8o"
+//   });
+//   const [map, setMap] = React.useState(null)
+//   const onLoad = React.useCallback(function callback(map) {
+//     setMap(map)
+//   }, [])
+//   const onUnmount = React.useCallback(function callback(map) {
+//     setMap(null)
+//   }, [])
+//   // Load Location for Marker
+//   let loc = [];
+//   for (let i = 0; i < locId.length; i++) {
+//     var url = "http://localhost:80/location/" + locId[i];
+//     fetch(url)
+//       .then((response) => {
+//         response.json()
+//       })
+//       .then((data) => {
+//         for (let i = 0; i < 15; i++) {
+//           let obj = { latitude: data[locId[i] - 1].latitude, longtitude: data[locId[i] - 1].longtitude };
+//           loc.push(obj);
+//         }
+//       })
+//   }
+//   console.log(loc);
+//   // Load Google Map
+//   return isLoaded ? (
+//     <div style={{ position: "absolute", right: "0", bottom: "0" }}>
+//       <GoogleMap
+//         mapContainerStyle={containerStyle}
+//         center={center}
+//         zoom={zoomSize}
+//         onLoad={onLoad}
+//         onUnmount={onUnmount}>
+//         <Marker position={{ lat: 22.356311, lng: 114.124516 }}></Marker>
+//         {loc.map((loc) => {
+//           // Place Marker on Map
+//           return (<Marker position={{ lat: loc.latitude, lng: loc.longtitude }}></Marker>);
+//         })}
+//       </GoogleMap>
+//     </div >
+//   ) : <></>
+// }
 
 // create location post form
-export class CreateLocation extends React.Component {
+class CreateLocation extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -133,7 +124,7 @@ export class CreateLocation extends React.Component {
   submitHandler = (e) => {
     e.preventDefault()
     console.log(this.state)
-    axios.post('http://localhost:8080/admin/add/location', this.state)
+    axios.post('http://localhost:80/admin/add/location', this.state)
       .then(response => {
         console.log(response)
       })
@@ -201,7 +192,7 @@ class CreateUser extends React.Component {
   submitHandler = (e) => {
     e.preventDefault()
     console.log(this.state)
-    axios.post('http://localhost:8080/admin/add/location', this.state)
+    axios.post('http://localhost:80/admin/add/location', this.state)
       .then(response => {
         console.log(response)
       })
@@ -252,7 +243,13 @@ class ReadLocation extends React.Component {
     e.preventDefault()
     console.log(this.state)
 
-    axios.get('http://localhost:8080/admin/location/'+this.state.locID, {parmas:{locID : this.state.locID}})
+    axios.get('http://localhost:80/admin/location/'+this.state.locID, {parmas:{locID : this.state.locID}})
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {
+      console.log(error)
+    })
   }
   render() {
     const { locID } = this.state
@@ -291,6 +288,12 @@ class ReadUser extends React.Component {
     e.preventDefault()
     console.log(this.state)
     axios.get('http://localhost:8080/admin/user/'+this.state.userID, {parmas:{userID : this.state.userID}})
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {
+      console.log(error)
+    })
   }
 
   render() {
@@ -320,48 +323,94 @@ class ReadUser extends React.Component {
 class UpdateLocation extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { locID: ""}
-    
+    this.state = { 
+      locID: '',
+      name: '',
+      latitude: '',
+      longtitude: '',
+      maxTrafficSpeed: '',
+      minTrafficSpeed: ''
+    }
   }
   changeHandler = (e) => {
     this.setState({ [e.target.name]: e.target.value })
   }
+  loadHandler = (e) => {
+    axios.get('http://localhost:80/admin/location/'+this.state.locID, {parmas:{locID : this.state.locID}})
+    .then(response =>{
+      this.setState({ name : response.name })
+      this.setState({ latitude : response.latitude})
+      this.setState({ longtitude : response.longtitude})
+      this.setState({maxTrafficSpeed : response.maxTrafficSpeed})
+      this.setState({ minTrafficSpeed : response.minTrafficSpeed})
+
+})
+    .catch(error =>{
+      console.log(error)
+    })
+  }
   submitHandler = (e) => {
     e.preventDefault()
     console.log(this.state)
-
-    axios.put('http://localhost:8080/admin/update/location/:locID'+this.state.locID, {parmas:{locID : this.state.locID}})
-    .then(response => {
-      console.log(response)
-    })
-    .catch(error => {
-      console.log(error)
-    })
-
+    axios.post('http://localhost:80/admin/add/location', this.state)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
   render() {
-    const { locID } = this.state
+    const { locID, name, latitude, longtitude, maxTrafficSpeed, minTrafficSpeed } = this.state
     return (
-      <>
-        <div style={{position: "absolute", left: "22%", top: "12%" }}>
-          <form onSubmit={this.submitHandler} >
-            <label>
-              Location name you want to update:
-              <input type="text" name="locID" value={locID} onChange={this.changeHandler} />
+      <div style={{position: "absolute", left: "22%", top: "12%" }}>
+        <form onSubmit={this.submitHandler}>
+          
+        <div>
+            <label>LocationId for fetching data
+              <input type="text" name="locID" value={locID} onChange={this.changeHandler}/>
             </label>
-            <button type="submit">Submit</button>
-          </form>
-        </div>
+          </div>
 
-   
-      </>
+          <button type="button " onClick={this.loadHandler}>Submit</button>
+          <div>
+            <label>
+              Name: 
+              <input type="text" name="name" value={name} onChange={this.changeHandler} />
+            </label>
+          </div>
+          <div>
+            <label>
+              Latitude: 
+              <input type="text" name="latitude" value={latitude} onChange={this.changeHandler} />
+            </label>
+          </div>
+          <div>
+            <label>Longtitude:
+              <input type="text" name="longtitude" value={longtitude} onChange={this.changeHandler} />
+            </label>
+          </div>
+          <div>
+            <label>MaxTrafficSpeed:
+              <input type="text" name="maxTrafficSpeed" value={maxTrafficSpeed} onChange={this.changeHandler} />
+            </label>
+          </div>
+          <div>
+            <label>MinTrafficSpeed:
+              <input type="text" name="minTrafficSpeed" value={minTrafficSpeed} onChange={this.changeHandler} />
+            </label>
+          </div>
 
+          <button type="submit">Submit</button>
+
+        </form>
+
+      </div>
 
 
     )
   }
 }
-
 
 class UpdateUser extends React.Component {
   constructor(props) {
@@ -380,7 +429,7 @@ class UpdateUser extends React.Component {
     e.preventDefault()
     console.log(this.state)
 
-    axios.put('http://localhost:8080/admin///update/user/:userID', {parmas:{locID : this.state.username}})
+    axios.put('http://localhost:80/admin///update/user/:userID', {parmas:{locID : this.state.username}})
     .then(response => {
       console.log(response)
     })
@@ -396,7 +445,7 @@ class UpdateUser extends React.Component {
         <div style={{position: "absolute", left: "22%", top: "12%" }}>
           <form onSubmit={this.submitHandler} >
             <label>
-              User name you want to update:
+              User name you want to :
               <input type="text" name="locID" value={username} onChange={this.changeHandler} />
             </label>
             <label>
@@ -432,7 +481,7 @@ class DeleteLocation extends React.Component {
     e.preventDefault()
     console.log(this.state)
 
-    axios.delete('http://localhost:8080/admin/delete/location/'+this.state.locID, {parmas:{locID : this.state.locID}})
+    axios.delete('http://localhost:80/admin/delete/location/'+this.state.locID, {parmas:{locID : this.state.locID}})
     .then(response => {
       console.log(response)
     })
@@ -476,7 +525,7 @@ class DeleteUser extends React.Component {
     e.preventDefault()
     console.log(this.state)
 
-    axios.delete('http://localhost:8080/admin/delete/location/'+this.state.userID, {parmas:{locID : this.state.userID}})
+    axios.delete('http://localhost:80/admin/delete/location/'+this.state.userID, {parmas:{locID : this.state.userID}})
     .then(response => {
       console.log(response)
     })
@@ -503,16 +552,3 @@ class DeleteUser extends React.Component {
     )
   }
 }
-
-
-
-
-
-
-
-const root = ReactDOM.createRoot(document.querySelector('#app'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
